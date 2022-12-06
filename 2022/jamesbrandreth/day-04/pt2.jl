9bebadac64220af75e409717b1cfe51d17ad57a9
James Brandreth <james.brandreth@gmail.com>
2022-12-06 13:49:19 +0000

    day-04 pt-2

---
+++ 2022/jamesbrandreth/day-04/pt2.jl
@@ -0,0 +1,19 @@
+function makeRange(s::String)
+    first, last = split(s, "-")
+    return parse(Int64, first):parse(Int64, last)
+end
+
+global total = 0
+open("input.txt", "r") do file
+    for line in eachline(file)
+        range_1, range_2 = split(line,",")
+        set_1 = Set(makeRange(String(range_1)))
+        set_2 = Set(makeRange(String(range_2)))
+
+        if !isdisjoint(set_1, set_2)
+            global total += 1
+        end
+    end
+end
+
+println(total)
