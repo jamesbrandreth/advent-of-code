2aa61607e4be24edc76e834af5135e135cdeadf6
nathans <nathans@carallon.com>
2022-12-07 23:25:33 +0000

    Complete day 6 part 2

--- 2022/nathans106/src/bin/day-06/main.rs
+++ 2022/nathans106/src/bin/day-06/main.rs
@@ -1,20 +1,22 @@
 use itertools::Itertools;
 
-fn marker_position(data: &str) -> usize {
+fn marker_position(data: &str, marker_size: usize) -> usize {
     let chars = data.chars().collect_vec();
     let window_idx = chars
-        .windows(4)
+        .windows(marker_size)
         .enumerate()
         .find(|(_idx, window)| window.iter().all_unique())
         .unwrap()
         .0;
-    window_idx + 4
+    window_idx + marker_size
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    let marker = marker_position(input);
-    println!("Start-of-packet marker is at {marker}");
+    let four_marker = marker_position(input, 4);
+    println!("Start-of-packet 4 marker is at {four_marker}");
+    let fourteen_marker = marker_position(input, 14);
+    println!("Start-of-packet 14 marker is at {fourteen_marker}");
 }
 
 #[cfg(test)]
@@ -32,7 +34,7 @@ mod tests {
         ];
 
         let expected: Vec<usize> = vec![7, 5, 6, 10, 11];
-        let result: Vec<usize> = inputs.iter().map(|s| marker_position(s)).collect();
+        let result: Vec<usize> = inputs.iter().map(|s| marker_position(s, 4)).collect();
         assert_eq!(expected, result);
     }
 }
